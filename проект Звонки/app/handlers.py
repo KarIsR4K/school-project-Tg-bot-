ef33102848eed96fe1fbcec9239ab9ba12277c09
KarIsR4K <skorpion249910@gmail.com>
2025-04-21 19:13:05 +0300

    handlers.py

--- проект Звонки/app/handlers.py
+++ проект Звонки/app/handlers.py
@@ -2,18 +2,25 @@ from aiogram import F, Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart, Command
 import app.keyboards as kb
-from app.modul_time import Les_time, make_text_for_time
+from app.modul_time import get_lesson_time
 
 router = Router()
 
 
-@router.message(CommandStart('start'))
-async def cmd_start(message: Message):
-    await message.answer('Привет! Чем могу помочь?')
-
-
 @router.message(Command('time'))
 async def cmd_time(message: Message):
-    text = make_text_for_time()
+    text = get_lesson_time()
     await message.answer(text=text, reply_markup=kb.main)
+    
+
+@router.message(Command("start"))
+async def cmd_start(message: Message):
+    user_guide = (
+        "Добро пожаловать в бота-помощника для отслеживания времени уроков!\n\n"
+        "Инструкция по использованию:\n"
+        "- Нажмите /start, чтобы увидеть это руководство.\n"
+        "- Нажмите /time, чтобы узнать, сколько времени осталось до начала или конца урока.\n\n"
+        "Бот использует стандартное расписание уроков. Если уроки закончились, бот сообщит об этом."
+    )
+    await message.answer(user_guide, reply_markup=get_lesson_time())
 
